7dcb52fa7a512c1df7149065d9b49917074d2a9b
Anurag <hazru.anurag@gmail.com>
2021-06-13 20:35:30 +0530

    chore: added support for puppeteer

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const fs = require("fs");
 const { config } = require("dotenv");
 const { TwitterClient } = require("twitter-api-client");
+const jokesData = require("./jokes.json");
+const Handlebars = require("handlebars");
+const puppeteer = require("puppeteer");
 
 config();
 
@@ -25,7 +28,26 @@ function readBase64File(path) {
   return fs.readFileSync(path, { encoding: "base64" });
 }
 
+function random(min, max) {
+  return Math.floor(min + Math.random() * max);
+}
+
 (async function () {
+  const randomJoke = jokesData.jokes[random(0, jokesData.jokes.length - 1)];
+
+  const template = Handlebars.compile(
+    fs.readFileSync("./template/index.html", { encoding: "utf-8" })
+  );
+
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  const page = await browser.newPage();
+  page.setViewport({ width: 1500, height: 500 });
+  await page.setContent(template({ q: randomJoke.q, a: randomJoke.a }));
+  await page.screenshot({ path: "test.png" });
+  await browser.close();
+
   const data = readBase64File("./test.png");
 
   await updateProfileBanner(data);
